8b15d687969e98cd840edbd99752a7ff59b44dd2
mayur.koli <mayur.koli@bookmyshow.com>
2022-12-09 19:53:59 +0530

    Initial Commit

---
+++ PassDataBetweenVCs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassDataBetweenVCs
+//
+//  Created by Mayur Koli on 09/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
